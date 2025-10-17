62ea2a7f380a54413ec4ca40a4cb30bc6ad91300
Julius Formilles <formillesjuliusandrei@gmail.com>
2025-10-17 19:29:26 +0800

    Deleted python notebook file

---
+++ main.py
@@ -0,0 +1,28 @@
+
+from typing import Union
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+items = []
+
+@app.get("/")
+def read_hello():
+    return{"Hello": "Hi"}
+
+@app.post("/items")
+def create_item(item: str):
+    items.append(item)
+    return items
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code = 404, detail=f"Item '{item_id}' not Found")
+
+
+
+
+
